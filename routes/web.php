b373769081df5ed62903cd634373eaafa670214f
wphkweerasinghe <hkalhara777@gmail.com>
2021-09-09 19:20:44 +0530

    create html file for each file and route them, fixed error redirect to home

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
-Route::get('/post',function (){
-    return view('post');
+Route::get('/posts/{post}',function ($slug){
+    $path = __DIR__."/../resources/views/post/{$slug}.html";
+
+    if (! file_exists($path)){
+        return redirect("/");
+    }
+    $post = file_get_contents($path);
+    return view('post',[
+        'post' => $post
+    ]);
 });
